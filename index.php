f25d177fd2b0292b3a0060c987a826508aaae6c6
maferl21 <74430399+maferl21@users.noreply.github.com>
2022-01-06 11:54:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
     <script src="https://kit.fontawesome.com/b965409e0d.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Directorio</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body> 
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <!--<table id="myTable" class="table table-dark"></table>--!>
         <table id="myTable" class="table table-dark"></table>
       </div>
     </div>
