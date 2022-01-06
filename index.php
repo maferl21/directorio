04d30e02ef581f46483d1621aeb3098e77059527
maferl21 <74430399+maferl21@users.noreply.github.com>
2022-01-06 11:43:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/stylesPrincipal.css">
+    <script src="https://kit.fontawesome.com/b965409e0d.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Directorio</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body> 
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <!--<table id="myTable" class="table table-dark"></table>--!>
+        <table id="myTable" class="table table-dark"></table>
+      </div>
+    </div>
+  </div>
+  
+  <script src="jquery/jquery-3.3.1.min.js"></script>
+  <script src="popper/popper.min.js"></script>
+  <script src="plugins/dynamicTable/jquery.dynamicTable-1.0.0.js"></script>
+  <script src="js/codigo.js"></script>
+  
+</body>
